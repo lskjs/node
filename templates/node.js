f9b9b1f262ef0edd1a4cc05da11860ae1364724b
Igor Suvorov <hi@isuvorov.com>
2023-01-13 15:56:21 +0300

    feat: new build system

--- templates/node.js
+++ templates/node.js
@@ -1,14 +1,9 @@
-const apk = ({ version } = {}) =>
-  version.includes('alpine')
-    ? 'apk add --no-cache'
-    : 'apt-get install --no-install-recommends -y';
-
 module.exports = ({ version }) =>
   `
 FROM node:${version}
 
 RUN \\
-    ${apk({ version })} curl && \\
+    apk add --no-cache curl && \\
     npm i -g pnpm@7 && \\
     pnpm config set store-dir .pnpm-store
 `.trimStart();
